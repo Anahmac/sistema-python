39126b9f931604bade4536e62f66e4843c12b296
Ana Flávia <a.flaviaa02@gmail.com>
2025-04-13 13:07:20 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 #Nome: Ana Flávia Maciel / Curso: Análise e desenvolvomento de sistemas 
  #Lista para estudantes
 estudantes = []
-invalid = "Essa opção ainda está em desenvolvimento"
-
 #Menu principal 
 def exibir_menu():
   print("Bem-vindo ao menu!")
@@ -19,88 +17,103 @@ def exibir_submenu():
   print ("2. Listar")
   print ("3. Atualizar")
   print ("4. Excluir")
-  print ("5. Excluir Todos")
   print("0. sair")
-
 #Incluir estudante
 def incluir_estudantes():
   nome = input("Digite o nome do estudante:")
-  estudantes.append(nome)
+  cpf = input("Digite o CPF do estudante:")
+  numero = input ("Digite o número de matrícula do estudante:")
+  estudante = {
+     "nome": nome,
+     "cpf": cpf,
+     "numero": numero
+  }
+  estudantes.append(estudante)
   print(f"Estudante: {nome}, adicionado com sucesso!")
-#Update estudante
-def update_estudantes():
-  entrada = input("Digite o número ou nome do estudante para remover: ")
-  if entrada.isdigit():
-    number = int(entrada)
-    if 0 <= number < len(estudantes):
-        upStudant = estudantes[number]
-        novo_nome = input("Digite o novo nome para o estudante: ")
-        estudantes[number] = novo_nome
-        print(f"Estudante atualizado com sucesso: {upStudant} -> {novo_nome}")
-    else:
-        print("Número inválido! Nenhum estudante com esse índice.")
-  else:
-    if entrada in estudantes:
-      novo_nome = input("Digite o novo nome para o estudante: ")
-      indice = estudantes.index(entrada)
-      estudantes[indice] = novo_nome
-      print(f"Estudante atualizado com sucesso: {upStudant} -> {novo_nome}")
-    else:
-      print("Por favor, digite um número ou nome válido.") 
-#Delete estudante 
-def delete_estudantes():
-  entrada = input("Digite o número ou nome do estudante para remover: ")
-  if entrada.isdigit():
-    number = int(entrada)
-    if 0 <= number < len(estudantes):
-        upStudant = estudantes[number]
-        del estudantes[number]
-        print(f"Estudante deletado com sucesso: {upStudant}")
-    else:
-        print("Número inválido! Nenhum estudante com esse índice.")
-  else:
-    if entrada in estudantes:
-      estudantes.remove(entrada)
-      print(f"Estudante '{entrada}' removido com sucesso!")
-    else:
-      print("Por favor, digite um número ou nome válido.")  
-#Clear all
-def delete_all_estudantes():
-  estudantes.clear()
-  print(f"Todos Estudantes removido com sucesso!")
+
+#Editar estudante
+def editar_estudante():
+   entrada = input("Digite o nome do estudante que deseja editar: ")
+   #verifica se há um estudante com esse nome 
+   nomes = [e["nome"]for e in estudantes]
+   if entrada in nomes:
+      indice = nomes.index(entrada)
+      estudante = estudantes[indice]
+      print(f"\nEstudante encontrado: {estudante}")
+
+      print("O que você deseja editar?")
+      print ("1. Nome")
+      print ("2. CPF")
+      print ("3. Matrícula")
+      print ("4. Editar tudo")
+
+      opçao = input("Digite o número da opção desejada:")
+      if opçao == "1" or opçao == "4":
+         novo_nome = input ("Novo nome: ")
+         estudante["nome"] = novo_nome
+
+      if opçao == "2" or opçao == "4":
+         novo_cpf = input("Novo CPF: ")
+         estudante["cpf"] = novo_cpf
+
+      if opçao == "3" or opçao == "4":
+         nova_matricula = input ("Nova matrícula: ")
+         estudante["numero"] = nova_matricula 
+
+         print("Estudante atualizado com sucesso!")
+         print(estudante)
 #Listar estudantes
 def listar_estudantes():
     if not estudantes:
       print("Ainda não há estudantes cadastrados")  
       return
-    print("Estudantes cadastrados:")
-    print(estudantes)
+    print("\nEstudantes cadastrados:")
+    for i, est in enumerate(estudantes, start=1):
+       print(f"{i}. Nome: {est['nome']}, CPF: {est['cpf']}, Matrícula: {est['numero']}")
+
+#Excluir estudante
+def excluir_estudante():
+   entrada = input("Digite o nome do estudante que deseja remover: ")
+   #verifica se há um estudante com esse nome 
+   nomes = [e["nome"]for e in estudantes]
+   if entrada in nomes:
+      indice = nomes.index(entrada)
+      estudante = estudantes[indice]
+      print(f"Estudante encontrado: {estudante}")
+      confirmaçao = input ("Tem certeza que deseja excluir o estudante? (sim/não):").lower()
+      
+      if confirmaçao == "sim":
+         estudantes.pop(indice)
+         print ("Estudante removido com sucesso!")
+      else:
+         print("Operação cancelada.")
+       
 #loop principal
 while True:
-        exibir_menu()
+      exibir_menu()
     #Coletar escolha do usuário
-        escolha = int (input("Digite o número da opção escolhida:"))
-        if escolha == 1:
+      escolha = int (input("Digite o número da opção escolhida:"))
+      if escolha == 1:
           print("Você escolheu: Estudantes")
-        elif escolha == 2:
-          print(invalid)
+      elif escolha == 2:
+          print("Essa opção ainda está em desenvolvimento")
           continue
-        elif escolha == 3:
-          print(invalid)
+      elif escolha == 3:
+          print("Essa opção ainda está em desenvolvimento")
           continue
-        elif escolha == 4:
-          print(invalid)
+      elif escolha == 4:
+          print("Essa opção ainda está em desenvolvimento")
           continue
-        elif escolha == 5:
-          print(invalid)
+      elif escolha == 5:
+          print("Essa opção ainda está em desenvolvimento")
           continue
-        elif escolha == 0:
+      elif escolha == 0:
           print("Encerrando programa")     
           break
-        else:
+      else:
           print("Escolha inválida, tente novamente.")  
           continue
-        if True:
+      if True:
             #loop secundário 
             #Submenu
               while True:
@@ -114,19 +127,11 @@ while True:
                     print("Vocês escolheu: Listar")
                     listar_estudantes()
                   elif escolha == 3:
-                    # print(invalid)
-                    print("Vocês escolheu: Atualizar")
-                    update_estudantes()
-                    continue
+                    print("Você escolheu: Atualizar")
+                    editar_estudante()
                   elif escolha == 4:
-                    # print(invalid)
-                    print("Vocês escolheu: Deletar")
-                    delete_estudantes()
-                    continue
-                  elif escolha == 5:
-                    # print(invalid)
-                    print("Vocês escolheu: Deletar Todos")
-                    delete_all_estudantes()
+                    print("Você escolheu: Excluir")
+                    excluir_estudante()
                     continue
                   elif escolha == 0:
                     print("Voltando ao menu principal")
